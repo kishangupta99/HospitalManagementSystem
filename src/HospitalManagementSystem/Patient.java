a2ad9d2d0cdb677983a7c19f92071a20219b1bab
kishangupta99 <kishankumar@gmail.com>
2025-04-04 18:37:24 +0530

    "first commit"

--- src/HospitalManagementSystem/Patient.java
+++ src/HospitalManagementSystem/Patient.java
@@ -1,4 +1,19 @@
 package HospitalManagementSystem;
 
+import java.sql.Connection;
+import java.util.Scanner;
+
 public class Patient {
+    private Connection connection;
+    private Scanner scanner;
+
+    private Patient(Connection connection, Scanner scanner){
+        this.connection =connection;
+        this .scanner = scanner;
+    }
+    public void addPatient(){
+        System.out.print("Enter Patient Name: ");
+        String name =scanner.next();
+
+    }
 }
